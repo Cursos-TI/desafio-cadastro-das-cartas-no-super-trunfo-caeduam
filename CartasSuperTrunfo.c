29125e84be273061279b88b82f5b78834e094a3d
caeduam <caeduam86@gmail.com>
2025-06-08 22:55:16 +0000

    Ajustes no estado e código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main (){
-    //Carta 1
+    
     char estado[20] = "São Paulo";
-    char codigocarta[5] = "SP01";
+    char codigocarta [4]= "A01";
     char cidade[20] = "Mogi das Cruzes";
     int populacao = 451;
     float areakm2 = 712;
     float pib = 19;
     int pontosturisticos = 20;
 
-    printf("Super Trunfo Países - carta 1- Digite a sigla do estado: ");
+    printf("Carta 1: pressione a tecla enter para mostrar");
     scanf("Digite o Estado: ");
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", codigocarta);
