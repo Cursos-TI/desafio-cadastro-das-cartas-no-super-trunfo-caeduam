c38dec25e7ba7c07def330e14ea8708e98d59d05
caeduam <caeduam86@gmail.com>
2025-06-08 23:09:43 +0000

    Inserção de comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,6 +2,7 @@
 
 int main (){
     
+    //inserir dados da cidade
     char estado[20] = "São Paulo";
     char codigocarta [4]= "A01";
     char cidade[20] = "Mogi das Cruzes";
@@ -10,14 +11,16 @@ int main (){
     float pib = 19;
     int pontosturisticos = 20;
 
+    //inserir os comandos para programação
     printf("Carta 1: pressione a tecla enter para mostrar");
-    scanf("Digite o Estado: ");
+    scanf("Digitar enter para começar: ");
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", codigocarta);
     printf("Cidade: %s\n", cidade);
     printf("População: %d mil habitantes \n", populacao);
-    printf("Área: %0.f mil km2 \n", areakm2);
-    printf("Pib: R$ %0.f bilhões \n", pib);
+    //digitar ".0" entre o %.0f para remover as casas decimais
+    printf("Área: %.0f mil km2 \n", areakm2);
+    printf("Pib: R$ %.0f bilhões \n", pib);
     printf("Pontos Turísticos: %d\n", pontosturisticos);
     printf("\n");
 
